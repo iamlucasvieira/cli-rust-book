652214cacf0ce21cc5578020d5e84f6552b89dc7
Lucas Vieira dos Santos <lucas6eng@gmail.com>
2024-05-20 16:10:13 +0200

    Add echor

---
+++ echor/tests/cli.rs
@@ -0,0 +1,41 @@
+use anyhow::Result;
+use assert_cmd::Command;
+use predicates::prelude::*;
+use std::fs;
+
+#[test]
+fn dies_no_args() -> Result<()> {
+    let mut cmd = Command::cargo_bin("echor")?;
+
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("Usage"));
+    Ok(())
+}
+
+fn run(args: &[&str], expected_file: &str) -> Result<()> {
+    let expected = fs::read_to_string(expected_file)?;
+    let mut cmd = Command::cargo_bin("echor")?;
+    cmd.args(args).assert().success().stdout(expected);
+    Ok(())
+}
+
+#[test]
+fn hello1() -> Result<()> {
+    run(&["Hello there"], "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello2() -> Result<()> {
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> Result<()> {
+    run(&["Hello there", "-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> Result<()> {
+    run(&["Hello", "there", "-n"], "tests/expected/hello2.n.txt")
+}
